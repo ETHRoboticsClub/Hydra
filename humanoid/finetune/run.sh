61c8851e8d1beb87465bb27fbcd48f8c74818f12
Luca <luca_frattini@outlook.com>
2026-03-11 15:20:24 +0100

    prepare for finetune big dataset

--- humanoid/finetune/run.sh
+++ humanoid/finetune/run.sh
@@ -102,13 +102,13 @@ CUDA_VISIBLE_DEVICES=0 uv run python \
     --num_gpus $NUM_GPUS \
     --output_dir "${CHECKPOINT_DIR}" \
     --save_total_limit 5 \
-    --save_steps 50 \
-    --max_steps 100 \
+    --save_steps 500 \
+    --max_steps 5000 \
     --warmup_ratio 0.05 \
     --weight_decay 1e-5 \
     --learning_rate 1e-4 \
-    --global_batch_size 32 \
-    --gradient_accumulation_steps 4 \
+    --global_batch_size 1024 \
+    --gradient_accumulation_steps 32 \
     --dataloader_num_workers 8 \
     --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08
 
